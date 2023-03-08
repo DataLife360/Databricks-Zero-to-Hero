d3b31fec496c272ca63a13addb73e10e4da0020b
blasa.matthew@yahoo.com <blasa.matthew@yahoo.com>
2023-03-08 19:09:13 +0000

    Intial Commit

---
+++ print_multiple_args.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# notebook1.py
+import sys
+
+# Access the first argument
+arg = sys.argv[1]
+
+# Print the argument
+print(arg)
+
+# Set the output of the notebook
+out = arg
+
+
+# COMMAND ----------
+
+
---
+++ run.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+print("Notebook worked")
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC 
+# MAGIC #Readmes
+# MAGIC 
+# MAGIC Readmes can also transfer to your new notebook
+
+# COMMAND ----------
+
+
